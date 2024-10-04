9404ee685c637e49a826843b87f62b79a4813409
kimjihye <hamiwood@naver.com>
2024-10-04 12:48:19 +0900

    docs : schedule.sql 수정

--- schedule.sql
+++ schedule.sql
@@ -1,11 +1,13 @@
 create table schedule
 (
     id          int auto_increment primary key,
-    email       varchar(50)                         not null,
-    todo        varchar(200)                        not null,
+    username    varchar(50) not null,
+    contents    varchar(200)                        not null,
     password    varchar(20)                         not null,
-    create_date timestamp default CURRENT_TIMESTAMP null,
-    update_date timestamp default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP
+    createdTime timestamp default CURRENT_TIMESTAMP null,
+    modifiedTime timestamp default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+    userid int,
+    foreign key (userid) references user(userid) on update cascade
 );
 
 create table user
